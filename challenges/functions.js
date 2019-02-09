c3401d21f86611b50f89ba2792bfeb1ccf23942e
Scott Schindler <scottschindler29@gmail.com>
2019-02-09 14:12:33 -0500

    done with objects and arrays. on to functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,23 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x,y,callback){
+  return callback(x,y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+function add() {
+  return callback(x + y)
+}
+
+function multiply(){
+  return callback(x*y)
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -122,43 +122,61 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
-/* Request 1: .forEach()
+// Request 1: .forEach()
 
-The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
+// The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
-*/
-const animalNames = [];
+
+let animalNames = [];
+
+zooAnimals.forEach(function(element) {
+
+  animalNames.push( `Name: ${element.animal_name}, Scientific: ${element.scientific_name}`)
+ 
+});
+
 console.log(animalNames);
 
-/* Request 2: .map()    
+//  Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+// The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
-*/
+let lowerCase = [];
+lowerCase = zooAnimals.map(x => x.animal_name.toLowerCase())
 
-const lowerCase = [];
 console.log(lowerCase); 
 
-/* Request 3: .filter() 
+//  Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
-
-*/
-const largerPopulation = [];
-console.log(largerPopulation);
-
-/* Request 4: .reduce() 
-
-The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
-
-*/
-const populationTotal = 0;
-console.log(populationTotal);
+// The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 
-/* 
+ let largerPopulation = [];
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+largerPopulation = zooAnimals.filter(x => x.population > 5)
 
-*/
+
+
+ console.log(largerPopulation);
+
+//  Request 4: .reduce() 
+
+// The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
+
+
+let populationTotal = [];
+
+for(i = 0; i < zooAnimals.length; i++){
+  populationTotal.push(zooAnimals[i].population)
+}
+ let total = populationTotal.reduce(function(acc,curr){
+return acc + curr
+
+});
+
+console.log(total);
+
+
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+
 
